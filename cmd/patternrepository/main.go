d9a5d5af2ec6330058d23c37b5ff7f9ae8e36287
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 21:34:45 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/patternrepository/main.go
@@ -0,0 +1,23 @@
+// cmd/patternrepository/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"patternrepository/internal/patternrepository"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := patternrepository.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
